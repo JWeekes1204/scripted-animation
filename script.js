b8cc8a37e19759199ed0e5c9471d33b738bfa9f0
Philip Makings <pmakings@salesforce.com>
2017-01-16 22:18:50 +0000

    changed to be road runner

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 $( document ).ready(function()
 {
-    $("body").keydown(function(event) {
-    //moves left
-    if (event.which === 37) {
-        $("#blue").css("left", $("#blue").offset().left - 10);
-    //moves right
-    } else if (event.which === 39) {
-       //your code here
-       
-    } else {
-        return;
-    }
-});
+    //sets element to the variable $pic
+var $pic = $("#pic"); 
+
+$pic.click(function(){
+   //Your code goes here
+    });
 });
