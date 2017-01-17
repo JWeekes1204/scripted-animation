db6bc4d349ca90dcb46579fca68dc90f324446bc
jweekes1204 <Jadenweekes1204@gmail.com>
2017-01-17 23:31:59 +0000

    made nice progress

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 $( document ).ready(function()
 {
-    //sets element to the variable $pic
-var $pic = $("#pic"); 
 
+var $pic = $("#pic");
+var ground = $('ground');
+var picTop = $("#pic").offset().top;
+var picBottom = picTop + $('#pic').height();
+var groundTop = $("#ground").offset().top;
+var groundBottom = groundTop + $('#ground').height();
 $pic.click(function(){
-   //Your code goes here
-    });
+    setInterval(function(){$pic.css("top", $pic.offset().top + 20);}, 1000);
+    
+  console.log("hello");
+});
+function myStopFunction() {
+    setInterval(function(){
+        if (picBottom >= groundTop) {
+            clearInterval(var $pic);
+            
+        }
+        $pic.css("top", $pic.offset().top + 20);
+        
+    }, 1000);
+}
 });
